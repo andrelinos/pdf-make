da6a67130aa9146b6e1feda554941b26edbc78e9
Andrelino Silva <andrelinodev@gmail.com>
2022-06-10 18:28:18 -0300

    Update fonts

--- src/components/pdfmake.tsx
+++ src/components/pdfmake.tsx
@@ -1,5 +1,5 @@
 import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 import { useEffect, useState } from 'react';
